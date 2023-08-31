29e7b43ac726eca6bd6cca78c78569063180ba94
Ruthchomette <143638019+Ruthchomette@users.noreply.github.com>
2023-08-30 23:31:16 -0400

    Update streamlit_app.py
    added more lines and some emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
 
 streamlit.title('my parent new healthy diner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & bueberry oatmeal')
-streamlit.text('kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled free-range Egg')
+streamlit.text('🥣omega 3 & bueberry oatmeal')
+streamlit.text('🥗kale, spinach & rocket smoothie')
+streamlit.text('🐔Hard-boiled free-range Egg')
+streamlit.text('🥑🍞Avocado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
