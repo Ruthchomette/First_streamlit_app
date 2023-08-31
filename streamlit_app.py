1bb56c913f5660bd45f7050baa3b93f61660978e
Ruthchomette <143638019+Ruthchomette@users.noreply.github.com>
2023-08-30 23:23:27 -0400

    Update streamlit_app.py
    added more lines in different forms, title, texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parent new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & bueberry oatmeal')
+streamlit.text('kale, spinach & rocket smoothie')
+streamlit.text('Hard-boiled free-range Egg')
