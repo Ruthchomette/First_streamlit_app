aace355d4d8ddf9c79195f770b498d64c60d2aa4
Ruthchomette <143638019+Ruthchomette@users.noreply.github.com>
2023-09-05 13:20:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('my parent new healthy diner')
+streamlit.title('my mom's new healthy diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favorites')
 streamlit.text('🥣omega 3 & bueberry oatmeal')
 streamlit.text('🥗kale, spinach & rocket smoothie')
 streamlit.text('🐔Hard-boiled free-range Egg')
